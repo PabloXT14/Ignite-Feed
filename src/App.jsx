2be408063f56dce0cee52774b1273bea97ae476f
Pablo Alan <pabloxt14@gmail.com>
2022-06-04 17:31:47 -0300

    feat: creating Sidebar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
 
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <div>
+            <p><strong>Diego Fernandes</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maxime ratione pariatur saepe deserunt praesentium.</p>
+          </div>
+          <div>
+            <p><strong>Diego Fernandes</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maxime ratione pariatur saepe deserunt praesentium.</p>
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import styles from './styles.module.css';
 
+import igniteLogo from '../../assets/ignite-logo.svg';
+
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>🧪 Ignite Feed</strong>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
         </header>
     );
 }
