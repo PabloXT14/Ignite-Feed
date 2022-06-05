81d1828a1a27aebeb59c6352caf591ed1456fdad
Pablo Alan <pabloxt14@gmail.com>
2022-06-05 16:38:52 -0300

    feat: Finished Sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './styles.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,14 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/pabloxt14.png" alt="User Profile" />
                 <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
